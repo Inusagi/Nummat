6351684a10e1e9394976c26be5341a1de6e8c67b
Inusagi <inoosagi@gmail.com>
2016-11-03 21:25:30 +0100

    fullførte den

--- Oving2_4.m
+++ Oving2_4.m
@@ -2,9 +2,12 @@ k = 0.25;
 omega = 1;
 A = .4;
 
+%lager y-vektor og t-vektor
 options = odeset('AbsTol',1.e-12,'RelTol', 1.e-12);
 [t, y] = ode45(@(t,y)[y(2);A*cos(omega*t)-k*y(2)+y(1).*(1-y(1).^2)],[0,100],[0;0],options);
 
+%plots
 figure(1)
-subplot(4,1,1)
 plot(t,y)
+figure(2)
+plot(y(:,1),y(:,2))
