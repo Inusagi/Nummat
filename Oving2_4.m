59afa69689dc5b2aea4f21d971c3b8f3567092ab
Inusagi <inoosagi@gmail.com>
2016-11-03 23:35:57 +0100

    La til kommentarer

--- Oving2_4.m
+++ Oving2_4.m
@@ -2,7 +2,7 @@ k = 0.25;
 omega = 1;
 A = .4;
 
-%lager y-vektor og t-vektor
+%lager y-vektor og t-vektor:
 options = odeset('AbsTol',1.e-12,'RelTol', 1.e-12);
 [t, y] = ode45(@(t,y)[y(2);A*cos(omega*t)-k*y(2)+y(1).*(1-y(1).^2)],[0,100],[0;0],options);
 
