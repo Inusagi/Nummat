f658d3f4de7b2d9dd0f86f846bd4b6e295191ce2
Inusagi <inoosagi@gmail.com>
2016-11-10 20:59:32 +0100

    ferdig

--- Oving7_5a.m
+++ Oving7_5a.m
@@ -2,10 +2,12 @@ A =  toeplitz([4 -1 0 -1 0 0]);
 A(4,3) = 0; A(3,4) = 0;
 b = [0; 5; 0; 6; -2; 6];
 
-omega = linspace(0,2);
-rhoB = zeros(1,100);
-for i=1:100
+N = 1000
+omega = linspace(0,2,N);
+rhoB = zeros(1,N);
+for i=1:N
     rhoB(i) = rhoSOR(A, omega(i));
 end
 
 plot(omega,rhoB)
+fprintf('Optimal omega = %1.4f\n',omega(find(rhoB==(min(rhoB)))))
